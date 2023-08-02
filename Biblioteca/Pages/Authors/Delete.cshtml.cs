3d323a4224d83bf1c80517a83e022e203bfcd04f
KEVIN DOS SANTOS SILVA <kevin.ssilva8@senacsp.edu.br>
2023-08-02 17:09:35 -0300

    Atualização dia 02/08/2023

---
+++ Biblioteca/Pages/Authors/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Data;
+using Biblioteca.Models;
+
+namespace Biblioteca.Pages.Authors
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Biblioteca.Data.BibliotecaContext _context;
+
+        public DeleteModel(Biblioteca.Data.BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public author author { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Author == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Author.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                author = author;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Author == null)
+            {
+                return NotFound();
+            }
+            var author = await _context.Author.FindAsync(id);
+
+            if (author != null)
+            {
+                author = author;
+                _context.Author.Remove(author);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
